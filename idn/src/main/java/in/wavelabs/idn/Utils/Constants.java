64c0a11f06ccfdad51a91e621ba8cbe066b043b2
Vivek Kiran <vivekkiran@Viveks-MacBook-Pro.local>
2016-06-19 19:25:11 +0530

    fix endpoint

--- idn/src/main/java/in/wavelabs/idn/Utils/Constants.java
+++ idn/src/main/java/in/wavelabs/idn/Utils/Constants.java
@@ -4,6 +4,6 @@ package in.wavelabs.idn.utils;
  * Created by Vivek on 6/22/15.
  */
 public class Constants {
-    public static final String MAIN = "http://10.9.9.6:8080/";
+    public static final String MAIN = "http://api.qa1.nbos.in/";
 }
 
